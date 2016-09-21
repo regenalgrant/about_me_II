182074165825e5a442b04c2498ecb79ccad20f51
regenalgrant <regenal@mac.com>
2016-09-21 08:56:13 -0700

    add new

---
+++ app.js
@@ -0,0 +1,41 @@
+var answer=' '
+var statement=('Answer yes or no');
+var answer=prompt('Did I serve in the Army?');
+
+if ( answer == 'yes') {
+   statement='Good Job!'
+} else {
+  (answer == 'no')
+ statement=('You can/'t tell'!')
+alert(statement)
+}
+
+  var answer=prompt('Did live all over europe?')
+if ( answer == 'yes')
+
+   statement='Hell fire boy!'
+
+if(answer == 'no')
+ {statement='Your nuts!'}
+alert(statement)
+
+var answer=prompt('Did I attend alot of schooling?')
+if ( answer == 'yes')
+statement='That/'s correct!'
+if(answer == 'no')
+{statement='Not bright are you!'}
+alert(statement)
+
+var answer=prompt('Did I deploy overseas?')
+if (answer == 'yes')
+statement='That/'s correct!'
+if(answer == 'no')
+statement='Man, you have problems!'
+alert(statement)
+
+var answer=prompt('Do I live in Portland?')
+if ( answer == 'yes')
+{statement='Well hell, no!'}
+if(answer == 'no')
+statement='I reside in Olympia!'
+alert(statement);
