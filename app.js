f9ca421fbc2165e7260ba8718b81e1c64b7770eb
regenalgrant <regenal@mac.com>
2016-09-21 20:01:45 -0700

    add change2 app.js

--- app.js
+++ app.js
@@ -1,66 +1,59 @@
 'use strict';
-var answerOne = prompt('Did I retire from the Army?')
+var answerOne = prompt('Did I retire from the Army?');
   answerOne = answerOne.toLowerCase();
     if  (answerOne === 'yes' || answerOne === 'y') {
       alert('Hell fire boy!');
         console.log ('Hell fire boy!');
+} else {
+    alert ('Too Bad, your not very bright!');
+      console.log('Too Bad, your not very bright!')
 }
-else {
-	   alert ('Too Bad, your not very bright!');
-	     console.log('Too Bad, your not very bright!')
-}
-var answerDuece = prompt('Did I live overseas ?')
+var answerDuece = prompt('Did I live overseas ?');
   answerDuece = answerDuece.toLowerCase();
     if  (answerDuece === 'yes' || answerDuece === 'y') {
       alert('Hell fire boy!');
       console.log ('Hell fire boy!');
-}
-else {
-	alert ('Too Bad, your not very bright!');
-	console.log('Too Bad, your not very bright!')
+} else {
+    alert ('Too Bad, your not very bright!');
+      console.log('Too Bad, your not very bright!')
 }
 
 var answerTre = prompt('Did go to several colleges?')
-answerTre = answerTre.toLowerCase();
-if  (answerTre === 'yes' || answerTre === 'y') {
-  alert('Yay, Hell fire boy!');
-console.log ('Yay, Hell fire boy!');
-}
-else {
-	alert ('Too Bad, you suck!');
-	console.log('Too Bad, you suck!')
-}
-
-var answerFour = prompt('Do I have kids?')
-answerFour = answerFour.toLowerCase();
-if  (answerFour === 'yes' || answerFour === 'y') {
-  alert('Nice, Hell fire boy!');
-console.log ('Yay, Hell fire boy!');
-}
-else {
-	alert ('Too Bad, I wish!');
-	console.log('Too Bad, I wish!')
+  answerTre = answerTre.toLowerCase();
+    if  (answerTre === 'yes' || answerTre === 'y') {
+      alert('Yay, Hell fire boy!');
+        console.log ('Yay, Hell fire boy!');
+} else {
+    alert ('Too Bad, you suck!');
+      console.log('Too Bad, you suck!')
 }
 
-var answerFour = prompt('Do I work currently?')
-answerFour = answerFour.toLowerCase();
-if  (answerFour === 'yes' || answerFour === 'y') {
-  alert('Its boring, Hell fire boy!');
-console.log ('Yay, Hell fire boy!');
+var answerFour = prompt('Do I have kids?');
+  answerFour = answerFour.toLowerCase();
+    if  (answerFour === 'yes' || answerFour === 'y') {
+      alert('Nice, Hell fire boy!');
+        console.log ('Yay, Hell fire boy!');
+} else {
+    alert ('Too Bad, I wish!');
+      console.log('Too Bad, I wish!')
 }
-else {
-	alert ('hmmm, I wish!');
-	console.log('hmmm, I wish!')
 
+var answerFour = prompt('Do I work currently?');
+  answerFour = answerFour.toLowerCase();
+    if  (answerFour === 'yes' || answerFour === 'y') {
+      alert('Its boring, Hell fire boy!');
+        console.log ('Yay, Hell fire boy!');
+} else {
+    alert ('hmmm, I wish!');
+      console.log('hmmm, I wish!');
 }
-var answerFive = prompt('Do I live in Portland?')
-answerFive = answerFive.toLowerCase();
-if  (answerFive === 'no' || answerFive === 'n') {
-  alert('Your, crazy!');
-console.log ('Your, crazy!');
-}
-else {
-	alert ('Good answer!');
-	console.log('Good answer!')
 
+var answerFive = prompt('Do I live in Portland?');
+  answerFive = answerFive.toLowerCase();
+    if  (answerFive === 'no' || answerFive === 'n'); {
+      alert('Your, crazy!');
+        console.log ('Your, crazy!');
+} else {
+    alert ('Good answer!');
+      console.log('Good answer!');
 }
